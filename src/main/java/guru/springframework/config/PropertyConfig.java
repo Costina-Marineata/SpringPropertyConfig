f8bf2e323e8f5b2c709a2d0f69dd81cf2791475a
cmarineata <costina.marineata@gmail.com>
2021-02-28 12:53:09 +0100

    get default prop. value from environment prop. instead of datasource.prop file

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -1,15 +1,20 @@
 package guru.springframework.config;
 
 import guru.springframework.examplebeans.FakeDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
+import org.springframework.core.env.Environment;
 
 @Configuration
 @PropertySource("classpath:datasource.properties")  //is looking into the property file to solve the below strings
 public class PropertyConfig {
+
+    @Autowired
+    Environment env;  // in EditConfiguration can be added your own environment param. ( ex: USERNAME with a specific value) The Autowire will override the local prop variable with the default ones
 
     @Value("${guru.user}") // when we use the @Value it will match the values that we created in the properties file (datasource.properties)
     String user;
@@ -23,6 +28,7 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
+//        fakeDataSource.setUser(env.getProperty("USERNAME"));  //*** Dvalue taken from the environment properties
         fakeDataSource.setUser(user);
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
