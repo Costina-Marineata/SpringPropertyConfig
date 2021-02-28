fde18faf86dda125190b97eb8b4e9f51f4dd5f8c
cmarineata <costina.marineata@gmail.com>
2021-02-28 13:11:37 +0100

    use spring boot application.properties files

--- src/main/java/guru/springframework/config/PropertyConfig.java
+++ src/main/java/guru/springframework/config/PropertyConfig.java
@@ -2,27 +2,12 @@ package guru.springframework.config;
 
 import guru.springframework.examplebeans.FakeDataSource;
 import guru.springframework.examplebeans.FakeJmsBroker;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
-import org.springframework.context.annotation.PropertySources;
-import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
-import org.springframework.core.env.Environment;
 
 @Configuration
-//@PropertySource({"classpath:datasource.properties", "classpath:jms.properties"})  //is looking into the property file to solve the below strings
-
-/**We can use PropertySource annotation added in spring4 as follows */
-@PropertySources({
-        @PropertySource("classpath:datasource.properties"),
-        @PropertySource("classpath:jms.properties")
-})
 public class PropertyConfig {
-
-    @Autowired
-    Environment env;  // in EditConfiguration can be added your own environment param. ( ex: USERNAME with a specific value) The Autowire will override the local prop variable with the default ones
 
     @Value("${guru.user}") // when we use the @Value it will match the values that we created in the properties file (datasource.properties)
     String user;
@@ -45,7 +30,6 @@ public class PropertyConfig {
     @Bean
     public FakeDataSource fakeDataSource(){
         FakeDataSource fakeDataSource = new FakeDataSource();
-//        fakeDataSource.setUser(env.getProperty("USERNAME"));  //*** Dvalue taken from the environment properties
         fakeDataSource.setUser(user);
         fakeDataSource.setPassword(password);
         fakeDataSource.setUrl(url);
@@ -61,11 +45,5 @@ public class PropertyConfig {
         fakeJmsBroker.setJmsUrl(jmsUrl);
 
         return fakeJmsBroker;
-    }
-
-    @Bean
-    public static PropertySourcesPlaceholderConfigurer properties(){
-        PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer = new PropertySourcesPlaceholderConfigurer();
-        return propertySourcesPlaceholderConfigurer;   //returnes a simple bean
     }
 }
